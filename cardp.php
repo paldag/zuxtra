69708f8b9631c2d99aa515ae1d316d3f4f4f8cc5
paldag <pallabd@gmail.com>
2016-06-17 15:20:47 +0530

    Create cardp.php

---
+++ cardp.php
@@ -0,0 +1,19 @@
+<?PHP
+session_start();
+error_reporting(0);
+include_once("connect.php");
+
+$sqli="select * from cardpay order by dt1 DESC";
+$q=mysql_query($sqli);
+while($rs=mysql_fetch_array($q))
+{
+?>
+<table><tr>
+<td><?=$rs[0];?></td>
+<td><?=$rs[1];?></td>
+<td><?=$rs[2];?></td>
+</tr>
+</table>
+<?PHP 
+}
+?>
